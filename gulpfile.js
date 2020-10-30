3da336aaaa553645834c9406fd22ad9f0188cdb4
Thomas Couvreur <t.couvreurthomas@gmail.com>
2020-10-30 09:15:22 +0100

    scss + gitignore

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict'; 
+const browserSync = require('browser-sync');
+const cleanCSS    = require("gulp-clean-css");
+const gulp        = require('gulp');
+const sass        = require('gulp-sass');
+sass.compiler     = require('node-sass');
+
+function makeCss() {
+  return gulp.src('./scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./www/css'));
+};
+
+function minimify(){
+  return gulp.src('./www/css/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./www/css'));
+}
+ 
+
+function watch(){
+  browserSync.init({
+    server: {
+      baseDir : "./www"
+    }
+  });
+  gulp.watch("./scss/*.scss", makeCss);
+  gulp.watch("./www/").on("change", browserSync.reload);
+}
+
+exports.watch = watch;
+exports.minimify = minimify;
