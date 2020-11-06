f0b1bc386d7c4ad1ec161aeb7e27f440acc61987
Thomas Couvreur <t.couvreurthomas@gmail.com>
2020-11-06 10:11:23 +0100

    tmp

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,22 @@ const browserSync = require('browser-sync');
 const cleanCSS    = require("gulp-clean-css");
 const gulp        = require('gulp');
 const sass        = require('gulp-sass');
-sass.compiler     = require('node-sass');
+// sass.compiler     = require('node-sass');
+const concat      = require('gulp-concat');
 
 function makeCss() {
   return gulp.src('./scss/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
     .pipe(gulp.dest('./www/css'));
 };
 
 function minimify(){
   return gulp.src('./www/css/*.css')
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./www/css'));
+    .pipe(gulp.dest('./www/css'))
+    ;
 }
- 
 
 function watch(){
   browserSync.init({
