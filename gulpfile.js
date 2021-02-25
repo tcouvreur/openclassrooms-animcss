98a5ed8e0801d80d33fde67281924333f0dc4789
Thomas Couvreur <t.couvreurthomas@gmail.com>
2021-02-26 00:12:20 +0100

    add animations, fix positions

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,27 @@
-'use strict'; 
-const browserSync = require('browser-sync');
-const cleanCSS    = require("gulp-clean-css");
-const gulp        = require('gulp');
-const sass        = require('gulp-sass');
+"use strict";
+const browserSync = require("browser-sync");
+const cleanCSS = require("gulp-clean-css");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
 // sass.compiler     = require('node-sass');
-const concat      = require('gulp-concat');
+const concat = require("gulp-concat");
 
 function makeCss() {
-  return gulp.src('./scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('./css'));
-};
-
-function minimify(){
-  return gulp.src('./css/*.css')
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('./css'))
-    ;
+  return gulp
+    .src("./scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(concat("style.css"))
+    .pipe(gulp.dest("./css"));
 }
 
-function watch(){
+function minimify() {
+  return gulp.src("./css/*.css").pipe(cleanCSS()).pipe(gulp.dest("./css"));
+}
+
+function watch() {
   browserSync.init({
     server: {
-      baseDir : "./"
+      baseDir: "./"
     }
   });
   gulp.watch("./scss/*.scss", makeCss);
